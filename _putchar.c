85a4b9451db02cc6c8ce02e583021209fc968e00
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2022-05-25 09:28:34 -0700

    Printf Team Project

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,7 +7,37 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c)
+int _putchar(int c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int pos;
+
+	if (c == -1 || pos >= 1024)
+	{
+		write(1, buf, pos);
+
+		pos = 0;
+	}
+	if (c != -1)
+	{
+		buf[pos++] = c;
+	}
+	return (1);
+}
+
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ * Return: number of characters printed
+ */
+
+int _puts(char *str)
+{
+	char *count = str;
+
+	while (*str)
+
+	_putchar(*str++);
+
+	return (str - count);
 }
